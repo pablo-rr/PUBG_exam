2c6330c66887526e1e1e944d465b224cd2595d4a
pablo-rr <pablorr2gmail.com>
2020-02-12 12:46:15 +0100

    team classes
    Signed-off-by: pablo-rr <pablorr2gmail.com>

---
+++ src/main/java/es/salesianos/model/AbstractTeam.java
@@ -0,0 +1,61 @@
+package es.salesianos.model;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("team")
+public class AbstractTeam implements Team{
+	private int maxMembers = 6;
+	private ArrayList<Pokemon> members = new ArrayList<Pokemon>();
+	
+	public int getMaxMembers() {
+		return maxMembers;
+	}
+
+	public void setMaxMembers(int maxMembers) {
+		this.maxMembers = maxMembers;
+	}
+
+	@Override
+	public void addMember(Pokemon newMember) {
+		if(members.size() < maxMembers) {
+			members.add(newMember);
+		}
+	}
+	
+	@Override
+	public void removeMember(String memberName) {
+		for (int i = 0; i < members.size(); i++) {
+			if(members.get(i).getName().equals(memberName)) {
+				members.remove(i);
+				return;
+			}
+		}
+	}
+	
+	@Override
+	public void removeMember(int memberIndex) {
+		members.remove(memberIndex);
+	}
+
+	@Override
+	public boolean isFull() {
+		if(members.size() < maxMembers) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+
+	@Override
+	public int getMemberCount() {
+		return members.size();
+	}
+
+	@Override
+	public ArrayList<Pokemon> getMembers() {
+		return members;
+	}
+}
